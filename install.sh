b0c4a638472822a6e628e6615f7439a2213019a0
colorsakura <2268136725@qq.com>
2019-02-17 02:07:40 +0800

    fix

--- install.sh
+++ install.sh
@@ -4,7 +4,15 @@ mv ~/.vimrc ~/.vimrc.bak
 
 touch ~/.vimrc
 
-echo "source ${PWD}/init.vim" > ~/.vimrc
+echo "source ~/.vim/init.vim" > ~/.vimrc
+
+ln -s ${PWD}/init.vim ~/.vim/init.vim
+ln -s ${PWD}/autoload ~/.vim/autoload
+ln -s ${PWD}/colors ~/.vim/colors
+ln -s ${PWD}/init ~/.vim/init
+ln -s ${PWD}/plugin ~/.vim/plugin
+ln -s ${PWD}/syntax ~/.vim/syntax
+ln -s ${PWD}/tools ~/.vim/tools
 
 curl -fLo ${PWD}/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
